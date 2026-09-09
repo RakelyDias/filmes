b34a3ff05030e64b2bc8a99b8ac4c623ed49eee4
RakelyDias <161658315+RakelyDias@users.noreply.github.com>
2026-09-09 11:14:04 -0300

    rota delete criada e funcionando

--- frontend/script.js
+++ frontend/script.js
@@ -1,20 +1,35 @@
 async function buscarFilmes() {
-    // acessar a rota GET do backend, trazer os filmes e mostrar os filmes na tela
-    const resposta = await fetch("https://filmes-vert-theta.vercel.app/") // resposta do backend
-    const filmes = await resposta.json() // converte a resposta num objeto JS
+    const resposta = await fetch("https://filmes-vert-theta.vercel.app/")
+    const filmes = await resposta.json()
     const sectionFilmes = document.querySelector(".filmes")
 
     filmes.forEach((filme) => {
-        console.log(filme)
         sectionFilmes.innerHTML += `
-                    <div>
-                        <h2>${filme.title}</h2>
-                        <p><strong>Gênero:</strong> ${filme.genre}</p>
-                        <p><strong>Duração:</strong> ${filme.duration} minutos</p>
-                        <p><strong>Classificação indicativa:</strong> ${filme.age_rating > 0 ? filme.age_rating + ' anos' : 'Livre'}</p>
-                    </div>
-                `
+            <div>
+                <h2>${filme.title}</h2>
+                <p><strong>Gênero:</strong> ${filme.genre}</p>
+                <p><strong>Duração:</strong> ${filme.duration} minutos</p>
+                <p><strong>Classificação indicativa:</strong> ${filme.age_rating > 0 ? filme.age_rating + ' anos' : 'Livre'}</p>
+
+                <button onclick="apagarFilme(${filme.id})">Apagar</button>
+            </div>
+        `
     })
 }
 
 buscarFilmes()
+
+async function apagarFilme(id) {
+    const respostaDeSucessoAoApagar = await fetch(
+        `https://filmes-vert-theta.vercel.app/delete/${id}`,
+        {
+            method: "DELETE"
+        }
+    )
+
+    const mensagem = await respostaDeSucessoAoApagar.json()
+
+    alert(mensagem.message)
+
+    window.location.reload()
+}
